5aed48b71de1e2468c84bfd3d096003e2c8da7f7
JWTappert <justin@theflybook.com>
2018-05-17 21:57:42 -0700

    exercise #2 complete

--- exercises/node/2-subscribe-with-observer.js
+++ exercises/node/2-subscribe-with-observer.js
@@ -1,4 +1,18 @@
 // `data$` is an observable stream of 10 numbers.
-const data$ = require('./fixtures/1-data.js');
+const data$ = require("./fixtures/1-data.js");
 
 // TODO: create an observer to subscribe to `data$` and log out all values.
+
+const observer = {
+	next(x) {
+		console.log(x);
+	},
+	error(err) {
+		console.error(err);
+	},
+	complete() {
+		console.info("done");
+	}
+};
+
+data$.subscribe(observer);
