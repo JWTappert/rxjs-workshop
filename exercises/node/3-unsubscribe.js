47631e3f59eebf2b879ef8879e874f23c4ab3bad
JWTappert <justin@theflybook.com>
2018-05-17 22:04:21 -0700

    exercise #3 complete

--- exercises/node/3-unsubscribe.js
+++ exercises/node/3-unsubscribe.js
@@ -1,5 +1,10 @@
 // `data$` is an observable stream of 10 numbers.
-const data$ = require('./fixtures/1-data.js');
+const data$ = require("./fixtures/1-data.js");
 
 // TODO: Get the subscription and unsubscribe it after 1 second
 
+const subscription = data$.subscribe(x => console.log(x));
+
+setTimeout(() => {
+	subscription.unsubscribe();
+}, 1000);
