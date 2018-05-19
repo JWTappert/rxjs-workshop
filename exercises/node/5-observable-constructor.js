938af47b402da6195c84a0f92f90a209a9546b14
JWTappert <justin@theflybook.com>
2018-05-18 20:39:10 -0700

    Exercise #5

--- exercises/node/5-observable-constructor.js
+++ exercises/node/5-observable-constructor.js
@@ -1,11 +1,36 @@
-const Rx = require('rxjs/Rx');
+const Rx = require("rxjs/Rx");
 
 // TODO: create an observable with the Observable constructor that
 // emits the values 1, 2, 3 and completes.
-const source$; //<-- set me!
 
-source$.subscribe(
-  x => console.log(x),
-  err => console.error(err),
-  () => console.info('done')
-);
+// these next calls will occur synch
+const source$ = new Rx.Observable(observer => {
+	observer.next(1);
+	observer.next(2);
+	observer.next(3);
+	observer.complete();
+});
+
+console.log("start");
+source$.subscribe(x => console.log(x), err => console.error(err), () => console.info("done"));
+console.log("stop");
+
+// these next calls will be 'async', you make them async in some way
+const src$ = new Rx.Observable(observer => {
+	const id = setTimeout(() => {
+		observer.next(1);
+		observer.next(2);
+		observer.next(3);
+		observer.complete();
+	}, 2000);
+
+	// you must specify the tear down method that is run when unsubscribe is called
+	return () => {
+		console.log("unsubscribing");
+		clearTimeout(id);
+	};
+});
+
+console.log("start");
+src$.subscribe(x => console.log(x), err => console.error(err), () => console.info("done"));
+console.log("stop");
