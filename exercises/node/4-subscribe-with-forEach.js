04223b9bbdffe5f535e54f69570baf607046f316
JWTappert <justin@theflybook.com>
2018-05-18 20:20:17 -0700

    Exercise #4

--- exercises/node/4-subscribe-with-forEach.js
+++ exercises/node/4-subscribe-with-forEach.js
@@ -1,8 +1,10 @@
 // `data$` is an observable stream of 10 numbers.
-const data$ = require('./fixtures/1-data.js');
+const data$ = require("./fixtures/1-data.js");
 
 // TODO: Subscribe to `data$` and log out all values to console.
 
+// returns a promise for each next call
+data$.forEach(x => console.log(x)).then(() => console.log("done"), err => console.error(err));
 
 // NOTE: If `forEach` returns a promise, how an we unsubscribe?
 //   We can't (yet! perhaps in the future of Rx?)
