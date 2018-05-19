bafb9d2b4448562ca9fc1af0848296eec5cf37c0
JWTappert <justin@theflybook.com>
2018-05-18 20:47:59 -0700

    exercise #6

--- exercises/node/6-observable-ctor-resource.js
+++ exercises/node/6-observable-ctor-resource.js
@@ -1,11 +1,13 @@
-const Rx = require('rxjs/Rx');
-const Resource = require('./fixtures/6-Resource');
+const Rx = require("rxjs/Rx");
+const Resource = require("./fixtures/6-Resource");
 
 // TODO: create an observable with the Observable constructor that
 // creates a new resource, subscribes to it's "data" event, and tears it
 // down when it's done.
 
 /**
+ *  This represents some event emitting function or data source, in this exercise we are
+ *  wrapping that resource and 'next'ing its event data and then properly unsubscribing
 NOTE: `Resource` usage:
 
 const resource = new Resource(); // start the resource;
@@ -13,12 +15,19 @@ resource.addEventListener('data', handler); // listen for data events
 resource.removeEventListener('data', handler); // stop listening for data events
 */
 
-const source$; // <-- set me!
-const subscription = source$.subscribe(
-  x => console.log(x),
-  err => console.error(err),
-  () => console.info('done')
-);
+const source$ = new Rx.Observable(observer => {
+	const resource = new Resource();
+	const handler = x => observer.next(x);
+
+	resource.addEventListener("data", handler);
+
+	return () => {
+		console.log("unsubscribing");
+		resource.removeEventListener("data", handler);
+	};
+});
+
+const subscription = source$.subscribe(x => console.log(x), err => console.error(err), () => console.info("done"));
 
 setTimeout(() => subscription.unsubscribe(), 2100);
 
